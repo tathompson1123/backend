e2e069ae39b55e2fbe78a997c327dc798f0789aa
tathompson1123 <ty@thompsonsautodetailing.com>
2026-01-25 16:42:01 -0800

    Update billing.js

--- routes/billing.js
+++ routes/billing.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { authenticateToken } = require('../config/middleware');
-const { pool } = require('../database');
+const { pool } = require('../config/database');
 
 // Create Checkout Session
 router.post('/create-checkout-session', authenticateToken, async (req, res) => {
